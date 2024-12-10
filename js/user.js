785a868103c141909fc2d7fe225b49d7fbda29e1
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-12-10 19:06:51 +0700

    admin role attach

--- js/user.js
+++ js/user.js
@@ -1,2 +1,36 @@
+const admin = require("firebase-admin");
 const { initializeApp } = require('firebase-admin/app');
+const { getAuth } = require('firebase-admin/auth');
+const serviceAccount = require('./sdk/ddlib.json');
 
+initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+})
+
+const auth = getAuth()
+
+async function attachAdminRole(uid) {
+    await auth.setCustomUserClaims(uid, { admin: true })
+
+    process.exit(1)
+}
+async function createAdmin() {
+    const admin = await auth.createUser({
+        email: 'admin@gmail.com',
+        displayName: 'Admin',
+        password: 'password',
+    })
+
+    await attachAdminRole(admin.uid)
+}
+async function setupAdmin() {
+    try {
+        const admin = await auth.getUserByEmail('admin@gmail.com')
+
+        await attachAdminRole(admin.uid)
+    } catch (err) {
+        await createAdmin()
+    }
+}
+
+setupAdmin()
