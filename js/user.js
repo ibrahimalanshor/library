24bad0d67d20119fb20c536b1a29b1fb4d9c7775
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-12-17 20:56:39 +0700

    import user

--- js/user.js
+++ js/user.js
@@ -2,6 +2,8 @@ const admin = require("firebase-admin");
 const { initializeApp } = require('firebase-admin/app');
 const { getAuth } = require('firebase-admin/auth');
 const serviceAccount = require('./sdk/ddlib.json');
+const fs = require('fs')
+const { parse } = require('csv-parse')
 
 initializeApp({
     credential: admin.credential.cert(serviceAccount)
@@ -11,8 +13,6 @@ const auth = getAuth()
 
 async function attachAdminRole(uid) {
     await auth.setCustomUserClaims(uid, { admin: true })
-
-    process.exit(1)
 }
 async function createAdmin() {
     const admin = await auth.createUser({
@@ -32,5 +32,38 @@ async function setupAdmin() {
         await createAdmin()
     }
 }
+async function setupUsers() {
+    const parser = fs
+        .createReadStream(`${__dirname}/data/siswa.csv`)
+        .pipe(parse({ columns: true }));
 
-setupAdmin()
+    for await (const record of parser) {
+        const [name, birthdate, email] = Object.values(record)
+
+        try {
+            await auth.getUserByEmail(email)
+        } catch (err) {
+            if (err.code === 'auth/user-not-found') {
+                await auth.createUser({
+                    email,
+                    displayName: name,
+                    password: birthdate,
+                })
+            }
+        }
+    }
+}
+async function listAllUsers() {
+    const users = await getAuth().listUsers()
+
+    users.users.forEach(user => console.log(user))
+}
+
+async function run() {
+    await setupAdmin()
+    await setupUsers()
+
+    process.exit(1)
+}
+
+run()
