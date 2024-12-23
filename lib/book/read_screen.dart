c1248bfbf758bf775b55d3bd067c312013e169ce
ibrahim <70515810+ibrahimalanshor@users.noreply.github.com>
2024-12-23 14:50:55 +0700

    Update read_screen.dart

--- lib/book/read_screen.dart
+++ lib/book/read_screen.dart
@@ -20,7 +20,7 @@ class ReadBookPage extends StatelessWidget {
         pdfFileName,
         onDocumentLoadFailed: (details) {
           print('Gagal memuat PDF:');
-          print(details);
+          print(details.description);
         },
         onDocumentLoaded: (details) {
           print('PDF berhasil dimuat: halaman');
