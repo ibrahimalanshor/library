4c8cfe5c54b6b1b289fc900130abe26b0472460a
ibrahim <70515810+ibrahimalanshor@users.noreply.github.com>
2024-12-30 14:30:45 +0700

    Update read_screen.dart

--- lib/book/read_screen.dart
+++ lib/book/read_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
 
 class ReadBookPage extends StatelessWidget {
@@ -7,16 +8,48 @@ class ReadBookPage extends StatelessWidget {
 
   ReadBookPage({required this.title, required this.author});
 
+  Future<String> getPDFUrl(String filePath) async {
+    try {
+      final ref = FirebaseStorage.instance.ref().child(filePath);
+      return await ref.getDownloadURL();
+    } catch (e) {
+      print('Error getting PDF URL: $e');
+      return '';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    String pdfFileName = 'books/${title.replaceAll(' ', '_').toLowerCase()}.pdf';
-
+    String pdfFilePath = 'buku_pdf/${title.replaceAll(' ', '_').toLowerCase()}.pdf';
 
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),  // Judul sesuai dengan parameter title
+        title: Text(title), // Judul sesuai dengan parameter title
       ),
-      body: SfPdfViewer.network('https://cdn.syncfusion.com/content/PDFViewer/flutter-succinctly.pdf')
+      body: FutureBuilder<String>(
+        future: getPDFUrl(pdfFilePath),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          if (snapshot.hasError || snapshot.data == null || snapshot.data!.isEmpty) {
+            return Center(
+              child: Text('Failed to load PDF'),
+            );
+          }
+
+          String pdfUrl = snapshot.data!;
+          return SfPdfViewer.network(
+            pdfUrl,
+            onDocumentLoadFailed: (details) {
+              print('Failed to load PDF: ${details.error}');
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(content: Text('Failed to load PDF')),
+              );
+            },
+          );
+        },
+      ),
     );
   }
 }
